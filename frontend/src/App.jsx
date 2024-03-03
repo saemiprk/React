1068829e5a7a461045d7cfd0bb68ae1981f11981
Saemi Park <saemiprk@gmail.com>
2024-03-03 14:14:04 +0900

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className="App">
+    <h1 className='text-3xl font-bold underline'>
+      Hello World!
+    </h1>
+   </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NotAuthRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const notAuthRoutes = () => {
+  return (
+    <div>notAuthRoutes</div>
+  )
+}
+
+export default notAuthRoutes
