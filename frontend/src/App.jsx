c9723d2b204d6d3ce4194710625c471600986ec6
Saemi Park <saemiprk@gmail.com>
2024-03-03 14:30:56 +0900

    Route Dom 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import Navbar from './layout/Navbar'
+import Footer from './layout/Footer'
+
+function Layout(){
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <div className="App">
-    <h1 className='text-3xl font-bold underline'>
-      Hello World!
-    </h1>
-   </div>
+   <Routes>
+    <Route path='/' element={<Layout></Layout>}>
+      <Route index element={<LandingPage />} />
+      <Route path='login' element={<LoginPage />} />
+      <Route path='register' element={<RegisterPage />} />
+
+    </Route>
+   </Routes>
   )
 }
 
