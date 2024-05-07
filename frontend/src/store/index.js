1dae9f7e6622dfe5e7b8bf35d73f6eb37da10c9e
Saemi Park <saemiprk@gmail.com>
2024-05-07 19:41:51 +0900

    redux add

---
+++ frontend/src/store/index.js
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+export const rootReducer = combineReducers({
+    user: userReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    })
+})
+
+export const persistor = persistStore(store)
